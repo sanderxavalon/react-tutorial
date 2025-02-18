aa12905221f73bdf7a379e0ec70282379a95df9c
sanderxavalon <sanderxavalon@gmail.com>
2025-02-19 02:22:37 +0800

    Finish 07

--- 07-event-handler-7/src/App.tsx
+++ 07-event-handler-7/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Toolbar
+      onPlayMovie={() => alert("Playing!")}
+      onUploadImage={() => alert("Uploading!")}
+    />
+  );
 }
 
-export default App
+function Toolbar({ onPlayMovie, onUploadImage }: {onPlayMovie : () => void, onUploadImage : () => void}) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onUploadImage}>Upload Image</Button>
+    </div>
+  );
+}
+
+function Button({ onClick, children }: {onClick : () => void, children: React.ReactNode}) {
+  return <button onClick={onClick}>{children}</button>;
+}
