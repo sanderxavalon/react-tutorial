ea4b450fb9ec2f43efd0e5053bd8edb68dc1a5e3
sanderxavalon <sanderxavalon@gmail.com>
2025-03-30 22:45:24 +0800

    Add comment

--- 12-updating-objects-in-state-4/src/App.tsx
+++ 12-updating-objects-in-state-4/src/App.tsx
@@ -1,29 +1,46 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
+
+// 定義 Person 介面
+interface Person {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
 export default function Form() {
-  const [person, setPerson] = useState({
+  // 使用 Person 介面作為 state 型別
+  const [person, setPerson] = useState<Person>({
     firstName: 'Barbara',
     lastName: 'Hepworth',
     email: 'bhepworth@sculpture.com'
   });
 
-  function handleFirstNameChange(e) {
+  // 處理名字變更的函數
+  function handleFirstNameChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 覆蓋 firstName 屬性為新的值
       firstName: e.target.value
     });
   }
 
-  function handleLastNameChange(e) {
+  // 處理姓氏變更的函數
+  function handleLastNameChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 覆蓋 lastName 屬性為新的值
       lastName: e.target.value
     });
   }
 
-  function handleEmailChange(e) {
+  // 處理電子郵件變更的函數
+  function handleEmailChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 覆蓋 email 屬性為新的值
       email: e.target.value
     });
   }
--- 12-updating-objects-in-state-5/src/App.tsx
+++ 12-updating-objects-in-state-5/src/App.tsx
@@ -1,7 +1,21 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
+
+// 定義 Artwork 介面
+interface Artwork {
+  title: string;
+  city: string;
+  image: string;
+}
+
+// 定義 Person 介面
+interface Person {
+  name: string;
+  artwork: Artwork;
+}
 
 export default function Form() {
-  const [person, setPerson] = useState({
+  // 使用 Person 介面作為 state 型別
+  const [person, setPerson] = useState<Person>({
     name: 'Niki de Saint Phalle',
     artwork: {
       title: 'Blue Nana',
@@ -10,38 +24,56 @@ export default function Form() {
     }
   });
 
-  function handleNameChange(e) {
+  // 處理名字變更的函數
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 覆蓋 name 屬性為新的值
       name: e.target.value
     });
   }
 
-  function handleTitleChange(e) {
+  // 處理作品標題變更的函數
+  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 更新 artwork 物件
       artwork: {
+        // 使用展開運算符複製 artwork 物件的所有屬性
         ...person.artwork,
+        // 覆蓋 title 屬性為新的值
         title: e.target.value
       }
     });
   }
 
-  function handleCityChange(e) {
+  // 處理城市變更的函數
+  function handleCityChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 更新 artwork 物件
       artwork: {
+        // 使用展開運算符複製 artwork 物件的所有屬性
         ...person.artwork,
+        // 覆蓋 city 屬性為新的值
         city: e.target.value
       }
     });
   }
 
-  function handleImageChange(e) {
+  // 處理圖片變更的函數
+  function handleImageChange(e: ChangeEvent<HTMLInputElement>) {
     setPerson({
+      // 使用展開運算符複製 person 物件的所有屬性
       ...person,
+      // 更新 artwork 物件
       artwork: {
+        // 使用展開運算符複製 artwork 物件的所有屬性
         ...person.artwork,
+        // 覆蓋 image 屬性為新的值
         image: e.target.value
       }
     });
