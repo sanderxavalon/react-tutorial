85941831a531114473f151d8fd84a3c592ca8770
sanderxavalon <sanderxavalon@gmail.com>
2025-04-07 00:14:32 +0800

    add 13 lab

---
+++ 13-updating-array-in-state-lab-3/src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import AddTodo from './AddTodo';
+import TaskList, { Todo } from './TaskList';
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+];
+
+export default function TaskApp() {
+  const [todos, setTodos] = useState(
+    initialTodos
+  );
+
+  function handleAddTodo(title: string) {
+    todos.push({
+      id: nextId++,
+      title: title,
+      done: false
+    });
+  }
+
+  function handleChangeTodo(nextTodo: Todo) {
+    const todo = todos.find(t =>
+      t.id === nextTodo.id
+    );
+    if (todo) {
+      todo.title = nextTodo.title;
+      todo.done = nextTodo.done;
+    }
+  }
+
+  function handleDeleteTodo(todoId: number) {
+    const index = todos.findIndex(t =>
+      t.id === todoId
+    );
+    todos.splice(index, 1);
+  }
+
+  return (
+    <>
+      <AddTodo
+        onAddTodo={handleAddTodo}
+      />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
+}
---
+++ 13-updating-array-in-state-lab-3/src/TaskList.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+
+export interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+interface TaskListProps {
+  todos: Todo[];
+  onChangeTodo: (todo: Todo) => void;
+  onDeleteTodo: (id: number) => void;
+}
+
+interface TaskProps {
+  todo: Todo;
+  onChange: (todo: Todo) => void;
+  onDelete: (id: number) => void;
+}
+
+export default function TaskList({
+  todos,
+  onChangeTodo,
+  onDeleteTodo
+}: TaskListProps) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <Task
+            todo={todo}
+            onChange={onChangeTodo}
+            onDelete={onDeleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ todo, onChange, onDelete }: TaskProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          value={todo.title}
+          onChange={e => {
+            onChange({
+              ...todo,
+              title: e.target.value
+            });
+          }} />
+        <button onClick={() => setIsEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        <button onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={e => {
+          onChange({
+            ...todo,
+            done: e.target.checked
+          });
+        }}
+      />
+      {todoContent}
+      <button onClick={() => onDelete(todo.id)}>
+        Delete
+      </button>
+    </label>
+  );
+}
