4e91e9a95347c777f07773cda0e5445a487d362f
sanderxavalon <sanderxavalon@gmail.com>
2025-05-12 12:34:09 +0800

    Add 16-1 lab

---
+++ 16-sharing-state-between-components-lab/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function SyncedInputs() {
+  return (
+    <>
+      <Input label="First input" />
+      <Input label="Second input" />
+    </>
+  );
+}
+
+function Input({ label }: { label: string }) {
+  const [text, setText] = useState('');
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setText(e.target.value);
+  }
+
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={text}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
