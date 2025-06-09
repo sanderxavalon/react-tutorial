2d43da6846064d516f124900a043fc4d0e50cd67
sanderxavalon <sanderxavalon@gmail.com>
2025-06-09 13:43:28 +0800

    Add 20-1

---
+++ 20-scaling-up-with-reducer-and-context/src/TaskList.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import type { Task } from './App';
+
+export default function TaskList({
+  tasks,
+  onChangeTask,
+  onDeleteTask
+}: {
+  tasks: Task[];
+  onChangeTask: (task: Task) => void;
+  onDeleteTask: (taskId: number) => void;
+}) {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>
+          <Task
+            task={task}
+            onChange={onChangeTask}
+            onDelete={onDeleteTask}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ task, onChange, onDelete }: { task: Task, onChange: (task: Task) => void, onDelete: (taskId: number) => void }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          value={task.text}
+          onChange={e => {
+            onChange({
+              ...task,
+              text: e.target.value
+            });
+          }} />
+        <button onClick={() => setIsEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        {task.text}
+        <button onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={task.done}
+        onChange={e => {
+          onChange({
+            ...task,
+            done: e.target.checked
+          });
+        }}
+      />
+      {taskContent}
+      <button onClick={() => onDelete(task?.id!)}>
+        Delete
+      </button>
+    </label>
+  );
+}
