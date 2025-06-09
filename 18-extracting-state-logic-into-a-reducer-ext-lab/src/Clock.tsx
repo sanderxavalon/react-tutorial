5a04cb38342012c7083eab58828fd5f80b8a3681
sanderxavalon <sanderxavalon@gmail.com>
2025-06-09 13:20:18 +0800

    Add 18-ext-lab

---
+++ 18-extracting-state-logic-into-a-reducer-ext-lab/src/Clock.tsx
@@ -0,0 +1,14 @@
+export default function Clock({ time }: { time: Date }) {
+    const hours = time.getHours();
+    if (hours >= 0 && hours <= 6) {
+      document.getElementById('time')!.className = 'night';
+    } else {
+      document.getElementById('time')!.className = 'day';
+    }
+    return (
+      <h1 id="time">
+        {time.toLocaleTimeString()}
+      </h1>
+    );
+  }
+  
