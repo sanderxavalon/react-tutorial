00bc1348a5cb790383dff082d3681cf57efe3b92
sanderxavalon <sanderxavalon@gmail.com>
2025-09-01 14:01:19 +0800

    add 23 lab

---
+++ 23-synchronizing-with-effects-lab-2/src/MyInput.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from "react";
+
+export default function MyInput({ shouldFocus, value, onChange }) {
+  const ref = useRef(null);
+
+  // TODO: call focus() only if shouldFocus is true.
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
+  return <input ref={ref} value={value} onChange={onChange} />;
+}
