cea9f4364307399a6771505761bf32a1d35fa9d6
sanderxavalon <sanderxavalon@gmail.com>
2025-09-08 13:55:14 +0800

    Add 23-lab-2

---
+++ 23-synchronizing-with-effects-ext-lab-2/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Counter from "./Counter.jsx";
+
+export default function Form() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow((s) => !s)}>
+        {show ? "Hide" : "Show"} counter
+      </button>
+      <br />
+      <hr />
+      {show && <Counter />}
+    </>
+  );
+}
