31dc041b57008df2612cd9ef993874a4714b4e6c
sanderxavalon <sanderxavalon@gmail.com>
2025-12-08 13:46:00 +0800

    fix 25-lab

--- 25-custom-hooks-lab/src/App.jsx
+++ 25-custom-hooks-lab/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [on, setOn] = useState(true);
-
-  const turnOn = () => setOn(true);
-  const turnOff = () => setOn(false);
-  const toggle = () => setOn((prev) => !prev);
-
   return (
     <div className="container">
       <h1 className="title">USETOGGLE</h1>
 
       <div className="links">
-        <button className="linkBtn" onClick={turnOn}>
-          Turn On
-        </button>
-        <button className="linkBtn" onClick={turnOff}>
-          Turn Off
-        </button>
-        <button className="linkBtn" onClick={toggle}>
-          Toggle
-        </button>
+        <button className="linkBtn">Turn On</button>
+        <button className="linkBtn">Turn Off</button>
+        <button className="linkBtn">Toggle</button>
       </div>
 
-      <div className="toggleWrapper" onClick={toggle}>
-        <div className={`toggle ${on ? "active" : ""}`}>
+      <div className="toggleWrapper">
+        <div className={`toggle ${true ? "active" : ""}`}>
           <div className="indicator"></div>
         </div>
-        <span className="statusText">{on ? "On" : "Off"}</span>
+        <span className="statusText">{true ? "On" : "Off"}</span>
       </div>
     </div>
   );
