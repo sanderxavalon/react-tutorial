92ed96b695fd52de249031752183de02a553b964
sanderxavalon <sanderxavalon@gmail.com>
2025-12-15 13:14:04 +0800

    add 25-8

---
+++ 25-custom-hooks-8/src/ChatRoom.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useChatRoom } from './useChatRoom.jsx';
+import { showNotification } from './notifications.js';
+
+export default function ChatRoom({ roomId }) {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+  useChatRoom({
+    roomId: roomId,
+    serverUrl: serverUrl,
+    onReceiveMessage(msg) {
+      showNotification('New message: ' + msg);
+    }
+  });
+
+  return (
+    <>
+      <label>
+        Server URL:
+        <input value={serverUrl} onChange={e => setServerUrl(e.target.value)} />
+      </label>
+      <h1>Welcome to the {roomId} room!</h1>
+    </>
+  );
+}
---
+++ 25-custom-hooks-8/src/useChatRoom.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useEffectEvent } from 'react';
+import { createConnection } from './chat.js';
+
+export function useChatRoom({ serverUrl, roomId, onReceiveMessage }) {
+  const onMessage = useEffectEvent(onReceiveMessage);
+
+  useEffect(() => {
+    const options = {
+      serverUrl: serverUrl,
+      roomId: roomId
+    };
+    const connection = createConnection(options);
+    connection.connect();
+    connection.on('message', (msg) => {
+      onMessage(msg);
+    });
+    return () => connection.disconnect();
+  }, [roomId, serverUrl]);
+}
