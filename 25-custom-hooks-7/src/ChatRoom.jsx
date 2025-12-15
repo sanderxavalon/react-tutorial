b040440f859048766afd7c4f3454ae6ff693ad6e
sanderxavalon <sanderxavalon@gmail.com>
2025-12-15 13:07:35 +0800

    add 25-7

---
+++ 25-custom-hooks-7/src/ChatRoom.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { useChatRoom } from './useChatRoom.jsx';
+
+export default function ChatRoom({ roomId }) {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+  useChatRoom({
+    roomId: roomId,
+    serverUrl: serverUrl
+  });
+
+  return (
+    <>
+      <label>
+        Server URL:
+        <input value={serverUrl} onChange={e => setServerUrl(e.target.value)} />
+      </label>
+      <h1>Welcome to the {roomId} room!</h1>
+    </>
+  );
+}
---
+++ 25-custom-hooks-7/src/useChatRoom.jsx
@@ -0,0 +1,19 @@
+
+import { useEffect } from 'react';
+import { createConnection } from './chat.js';
+import { showNotification } from './notifications.js';
+
+export function useChatRoom({ serverUrl, roomId }) {
+  useEffect(() => {
+    const options = {
+      serverUrl: serverUrl,
+      roomId: roomId
+    };
+    const connection = createConnection(options);
+    connection.connect();
+    connection.on('message', (msg) => {
+      showNotification('New message: ' + msg);
+    });
+    return () => connection.disconnect();
+  }, [roomId, serverUrl]);
+}
