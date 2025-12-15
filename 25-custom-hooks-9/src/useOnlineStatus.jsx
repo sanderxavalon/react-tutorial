546a34389a88e457069c1c81f651cb63ed369389
sanderxavalon <sanderxavalon@gmail.com>
2025-12-15 13:16:23 +0800

    add 25-9

---
+++ 25-custom-hooks-9/src/useOnlineStatus.jsx
@@ -0,0 +1,19 @@
+import { useSyncExternalStore } from 'react';
+
+function subscribe(callback) {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  };
+}
+
+export function useOnlineStatus() {
+  return useSyncExternalStore(
+    subscribe,
+    () => navigator.onLine, // How to get the value on the client
+    () => true // How to get the value on the server
+  );
+}
+
